ea5bff61646743d2c0912e24fe66fd47c6369a8b
joshgit <jschermann91@gmail.com>
2021-11-04 18:34:36 -0500

    timer built

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-var countdown = $("#countdown");
+let countdown = $("#countdown");
+let timeLeft = 5;
 
-countdown.text(66);
+let tickTock = () => {
+  if (timeLeft) {
+    timeLeft--;
+    countdown.text(timeLeft);
+  } else {
+    //end game
+    console.log("end time");
+    clearInterval(interval);
+  }
+};
+
+// start timer
+let interval = setInterval(tickTock, 1000);
