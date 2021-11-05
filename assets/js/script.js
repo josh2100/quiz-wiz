9d38a9ac48530276ad71f3cd7ddaa57a5e9fb887
joshgit <jschermann91@gmail.com>
2021-11-05 09:16:39 -0500

    start page array

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,42 @@ let tickTock = () => {
 
 // start timer
 let interval = setInterval(tickTock, 1000);
+
+
+
+
+// let questionHolder = $("main").append("<p>Test</p>");
+// $("questionHolder").; add class col
+
+let startPageArray = [];
+
+startPageArray = $("main").html();
+
+alert(startPageArray);
+
+//remove start page function
+
+// build start page function
+
+// create array of objects for questions
+// questions [
+// {name:one
+// answer1: answer1,
+//answer2: answer 2
+// correctanswer:number}
+// ]
+
+/// create question function
+// generates a question
+// takes object properties as arguments
+// this.name then scramble this.answers
+// append child or whatever
+
+// check accuracy
+// when clicked, compare if this.correctanswer = submitted answer
+// stick clicked answer number and correctanswer in an array
+
+//.hide() .show()
+// sdafsadf
+// sdafsadfadsf
+// asdf
